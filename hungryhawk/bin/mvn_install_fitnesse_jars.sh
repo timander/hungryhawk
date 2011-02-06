87127279d892eee787c532104dc81b5d949d6f90
Tim Andersen <timander@gmail.com>
2011-02-06 14:25:43 -0600

    added bin/lib and mvn_install_fitnesse_jars.sh

---
+++ hungryhawk/bin/mvn_install_fitnesse_jars.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -DgroupId=org.fitnesse -DartifactId=fitnesse -Dversion=20100303 -Dpackaging=jar -DgeneratePom=true -Dfile=../fitnesse/fitnesse-20100103.jar
+mvn install:install-file -DgroupId=org.fitnesse -DartifactId=fitlibrary -Dversion=20090628 -Dpackaging=jar -DgeneratePom=true -Dfile=lib/fitlibrary-20090628.jar
